a269a2f184feb51677f85443cfa2e801c2572ac6
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2014-10-08 15:53:15 -0300

    Adding bin

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-;(function(args){
-    
-    console.log("Ornito Alert: This is an experimental tool! Please do not use it for production purposes");
-    
-    var cli = require("./lib");
-    var action = cli.parse(args);
-    var params = [].slice.call(args);
-
-    action.run(params);
-
-})(process.argv.slice(2));
+require("./lib/index");
